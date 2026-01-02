bbbce1a437d240a79e43b1ff21397364c4ab3815
Nugie kurniawan <nugiebinhar@gmail.com>
2026-01-02 23:56:40 +0700

    app:membuat routing untuk dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import {Route, Routes} from "react-router-dom";
 import GuestLayout from "./layouts/Guest.layout";
 import LoginPage from "./pages/Login.page";
 import RegisterPage from "./pages/Register.page";
+import DashboardPage from "./pages/Protected/Dashboard.page";
+import ProtectedLayout from "./layouts/Protected.layout";
 
 function App() {
     return (
@@ -10,6 +12,9 @@ function App() {
                 <Route element={<GuestLayout />}>
                     <Route path="/login" element={<LoginPage />} />
                     <Route path="/register" element={<RegisterPage />} />
+                </Route>
+                <Route element={<ProtectedLayout />}>
+                    <Route path="/dashboard" element={<DashboardPage />} />
                 </Route>
             </Routes>
         </>
